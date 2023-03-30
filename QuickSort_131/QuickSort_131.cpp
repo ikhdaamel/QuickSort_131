e7e4e4cd6214745d12b636d5858275c0d156a5d5
ikhdaamel <ikhda.new45@gmail.com>
2023-03-30 15:03:01 +0700

    Add project files.

---
+++ QuickSort_131/QuickSort_131.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+
+int arr[20];                //array of integres to hold values
+int cmp_cout = 0;           //number of comparasion
+int mov_cout = 0;           //number of data movements
+int n
+
+
+
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
