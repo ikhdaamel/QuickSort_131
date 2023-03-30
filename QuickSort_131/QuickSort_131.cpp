6e1a8b28304dd712a2a1c6b8b1ab48d160c71553
ikhdaamel <ikhda.new45@gmail.com>
2023-03-30 15:50:42 +0700

    lakukan pengulangan

--- QuickSort_131/QuickSort_131.cpp
+++ QuickSort_131/QuickSort_131.cpp
@@ -105,5 +105,11 @@ void display() {
     cout << "sorted array" << endl;
     cout << "============" << endl;
 
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
 
 }
+
